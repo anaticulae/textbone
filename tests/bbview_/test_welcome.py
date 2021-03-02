ce036cc74a14b83a5762db69d986294d0b167d4e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-02 02:08:05 +0100

    test(view): invoke landing page

---
+++ tests/bbview_/test_welcome.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import utilatest
+
+import tests.bbview_
+
+
+def test_welcome(client):
+    welcome = utilatest.get(client, '/')
+    assert '<h1>Home</h1>' in welcome
+
+
+def test_analysis(client):
+    welcome = utilatest.get(client, '/analysis')
+    assert '<h1>Analysis</h1>' in welcome
