98b787fd66580a0038fa77dbc657e64dd2155417
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-02 02:05:46 +0100

    test(config): setup API prefix and add test client

---
+++ tests/bbview_/conftest.py
@@ -0,0 +1,31 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import pytest
+import utilatest
+
+import bbview.app
+import bbview.config
+
+utilatest.setup(bbview.config.API_PREFIX)
+
+
+@pytest.fixture
+def app(testdir):
+    """Create and configure a new app instance for each test."""
+    appi = bbview.app.create_application()
+
+    with appi.app_context():
+        yield appi
+
+
+@pytest.fixture
+def client(app):  # pylint:disable=W0621
+    """A test client for the app."""
+    return app.test_client()
