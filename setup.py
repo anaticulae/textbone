93db26ce8e3ef86243a85b0e8763566d25e05bc6
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-07 01:13:55 +0000

    chore(setup): use quick install

--- setup.py
+++ setup.py
@@ -8,61 +8,31 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'backbone/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'backbone',
+    'backbone.judge',
+    'backbone.magic',
+    'backbone.train',
+    'bbview.api',
+    'bbview.api.analysis',
+    'bbview.api.data',
+    'bbview.api.judge',
+    'bbview.api_',
+    'bbview.api_.analysis',
+    'bbview.api_.judge',
+    'bbview.view',
+    'hugedata',
+    'judgeddata',
+]
+ENTRY_POINTS = dict(console_scripts=[
+    'normalize = backbone.normalize:main',
+    'ngram = backbone.ngram:main',
+])
 
 if __name__ == "__main__":
-    # allow setup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='corpus collection',
+    utila.install(
+        __file__,
         include_package_data=True,
-        install_requires=REQUIRES,
-        long_description=README,
-        name='backbone',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/backbone',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-        ],
-        packages=[
-            'backbone',
-            'backbone.judge',
-            'backbone.magic',
-            'backbone.train',
-            'bbview.api',
-            'bbview.api.analysis',
-            'bbview.api.data',
-            'bbview.api.judge',
-            'bbview.api_',
-            'bbview.api_.analysis',
-            'bbview.api_.judge',
-            'bbview.view',
-            'hugedata',
-            'judgeddata',
-        ],
-        entry_points={
-            'console_scripts': [
-                'normalize = backbone.normalize:main',
-                'ngram = backbone.ngram:main',
-            ],
-        },
     )
